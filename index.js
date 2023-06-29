c5045c33c62e6f0c67bd2aea08109f4676cddf02
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-06-29 10:21:15 -0700

    added inquirer library and included it and fs in consts

---
+++ index.js
@@ -0,0 +1,36 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
